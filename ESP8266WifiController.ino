7c1d09eecca5e15756a1f386b4d05d33b05c458b
Soynerdito <soynerdito@gmail.com>
2016-12-06 20:58:54 -0400

    initial commit

---
+++ ESP8266WifiController.ino
@@ -0,0 +1,66 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+const char* host = "esp8266-webupdate";
+
+void handleRoot() {
+  httpServer.send(200, "text/html", "<h1>You are connected</h1>");
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  wifiManager.resetSettings();
+
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  wifiManager.setTimeout(180);
+  
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if(!wifiManager.autoConnect("WifiChuchito")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  } 
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  //Do what we are intended to do   
+  MDNS.begin(host);
+  httpUpdater.setup(&httpServer);
+
+  httpServer.on("/", handleRoot);
+  httpServer.begin();  
+  MDNS.addService("http", "tcp", 80);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  httpServer.handleClient();
+}
